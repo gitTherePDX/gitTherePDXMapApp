6919b6320e14dd1ec53b07611805fec8a426d3a1
Tim Combs <tim00combs@gmail.com>
2016-10-12 09:57:50 -0700

    fix lyft-api

--- scripts/model/lyft-api.js
+++ scripts/model/lyft-api.js
@@ -6,7 +6,16 @@
 
   lyftObject.dataAll = [];
 
-  lyftObject.callApi = function() {
+  lyftObject.attachEta = function() {
+    var lyft = lyftObject.dataAll.filter(function(car){
+      return car.display_name === 'Lyft';
+    });
+    var eta = lyft[0].eta_seconds;
+    console.log('lyft eta from attachEta', eta);
+    return eta;
+  };
+
+  lyftObject.callApi = function(callback) {
     var longitude = '-122.6765';
     var latitude = '45.5231';
     var ajaxQuery = {
@@ -19,7 +28,7 @@
         var eta = lyftObject.attachEta();
 
         var etaTransform = etaObject.etaTransform(eta);
-        
+
         callback(etaObject.context, etaTransform, etaObject.canvas.clientHeight / 6, 'lyft-logo', etaObject.etaLogos);
       },
       error: function(jqXHR, textStatus, errorThrown) {
