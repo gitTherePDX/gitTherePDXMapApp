c58d9e1eb99f1d76a2f9e8f19cea2020332c8d3b
dchasepdx <dchasepdx@gmail.com>
2016-10-13 10:30:56 -0700

    deprecated navigator geolocation function until ssl encryption is up on our app

--- scripts/model/googleDataMapping.js
+++ scripts/model/googleDataMapping.js
@@ -73,26 +73,28 @@
         //uberObject.updateInfo(googleMapping.currentLocation);
         //lyftObject.updateInfo(googleMapping.currentLocation);
       });
-    } else if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        googleMapping.currentLocation = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-        if (filterData.Uber) {
-          uberObject.getInfo(etaObject.drawLogo, googleMapping.currentLocation);
-        }
-        if (filterData.Lyft) {
-          lyftObject.getInfo(etaObject.drawLogo,googleMapping.currentLocation);
-        }
-        if (filterData.Biketown) {
-          biketownObject.getStationInfo(mapViews.createMarkers);
-        };
-            //uberObject.updateInfo(googleMapping.currentLocation);
-            //lyftObject.updateInfo(googleMapping.currentLocation);
-      }, function() {
-        googleMapping.handleLocationError(true, googleMapping.map, googleMapping.map.getCenter());
-      });
+
+      /**TODO Navigator geolocation will be deprecated until ssl encryption can be used for this app**/
+    // } else if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(function(position) {
+    //     googleMapping.currentLocation = {
+    //       lat: position.coords.latitude,
+    //       lng: position.coords.longitude
+    //     };
+    //     if (filterData.Uber) {
+    //       uberObject.getInfo(etaObject.drawLogo, googleMapping.currentLocation);
+    //     }
+    //     if (filterData.Lyft) {
+    //       lyftObject.getInfo(etaObject.drawLogo,googleMapping.currentLocation);
+    //     }
+    //     if (filterData.Biketown) {
+    //       biketownObject.getStationInfo(mapViews.createMarkers);
+    //     };
+    //         //uberObject.updateInfo(googleMapping.currentLocation);
+    //         //lyftObject.updateInfo(googleMapping.currentLocation);
+    //   }, function() {
+    //     googleMapping.handleLocationError(true, googleMapping.map, googleMapping.map.getCenter());
+    //   });
 
 
     } else {
